42973b377782652140d1f82f2ed7c0a396dd5431
pha <15-rueDelapaix>
2020-05-19 01:52:03 +0200

    adding full text search

--- src/main/java/com/keywer/article/demospringelasticsearch/dao/TweetRepository.java
+++ src/main/java/com/keywer/article/demospringelasticsearch/dao/TweetRepository.java
@@ -12,6 +12,39 @@ public interface TweetRepository extends ElasticsearchRepository<Tweet, String> 
 
     Page<Tweet> findByUsernameOrderByCreationDate(String name, Pageable pageable);
 
+    /*
+    "query": {
+        "bool": {
+            "must": [
+                {
+                    "match": {
+                        "username": "?0"
+                    }
+                }
+            ]
+        }
+    }  */
     @Query("{\"bool\": {\"must\": [{\"match\": {\"username\": \"?0\"}}]}}")
     Page<Tweet> findByUsernameUsingCustomQuery(String name, Pageable pageable); // elasticsearch boolean query
+
+    /*
+    "query": {
+        "bool": {
+            "should": [
+                {
+                    "match": {
+                        "content": "?0"
+                    }
+                },
+                {
+                    "match": {"username": "?0"}
+                },
+                {
+                    "match": {"localization": "?0"}
+                }
+            ]
+        }
+    }  */
+    @Query("{\"bool\": {\"should\": [{\"match\": {\"content\": \"?0\"}}, {\"match\": {\"username\": \"?0\"}}, {\"match\": {\"localization\": \"?0\"}}]}}")
+    Page<Tweet> search(String text, Pageable pageable);
 }
--- src/main/java/com/keywer/article/demospringelasticsearch/service/TweetService.java
+++ src/main/java/com/keywer/article/demospringelasticsearch/service/TweetService.java
@@ -4,6 +4,7 @@ import com.keywer.article.demospringelasticsearch.dao.TweetRepository;
 import com.keywer.article.demospringelasticsearch.model.Tweet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,12 +16,24 @@ public class TweetService {
     private TweetRepository tweetRepository;
 
     public List<Tweet> findByUsername(String username, int pageNum, int pageSize) {
-        return tweetRepository.findByUsernameOrderByCreationDate(username, PageRequest.of(pageNum, pageSize))
+        return tweetRepository.findByUsernameOrderByCreationDate(username, pageableOf(pageNum, pageSize))
                 .getContent();
     }
 
-    public List<Tweet> findByUsernameUsingCustomQuery(String username, int pageNum, int pageSize) {
-        return tweetRepository.findByUsernameUsingCustomQuery(username, PageRequest.of(pageNum, pageSize))
+    public List<Tweet> findByUsernameUsingCustomQuery(String username, Integer pageNum, Integer pageSize) {
+        return tweetRepository.findByUsernameUsingCustomQuery(username, pageableOf(pageNum, pageSize))
                 .getContent();
+    }
+
+    public List<Tweet> search(String text, Integer pageNum, Integer pageSize) {
+        return tweetRepository.search(text, pageableOf(pageNum, pageSize))
+                .getContent();
+    }
+
+    private Pageable pageableOf(Integer pageNum, Integer pageSize) {
+        if (pageNum == null || pageSize == null) {
+            return Pageable.unpaged();
+        }
+        return PageRequest.of(pageNum, pageSize);
     }
 }
